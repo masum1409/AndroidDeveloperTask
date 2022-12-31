35b5e8f4c7d71f818bfa7ade0a4a9f560dec2206
Masum Hossain <masum14092@gmail.com>
2022-12-31 22:24:58 +0600

    setup domain shop repository

---
+++ app/src/main/java/com/masum/androiddevelopertask/domain/repository/ShopRepository.kt
@@ -0,0 +1,9 @@
+package com.masum.androiddevelopertask.domain.repository
+
+import com.masum.androiddevelopertask.data.model.Shop
+import com.masum.androiddevelopertask.data.util.Resource
+
+
+interface ShopRepository {
+    suspend fun  getAllProducts(): Resource<Shop>
+}
---
+++ app/src/main/java/com/masum/androiddevelopertask/domain/usecase/ProductUseCase.kt
@@ -0,0 +1,13 @@
+package com.masum.androiddevelopertask.domain.usecase
+
+import com.masum.androiddevelopertask.data.model.Shop
+import com.masum.androiddevelopertask.data.util.Resource
+import com.masum.androiddevelopertask.domain.repository.ShopRepository
+import javax.inject.Inject
+
+class ProductUseCase @Inject constructor(
+    private val repository: ShopRepository
+){
+    suspend fun getAllProducts() : Resource<Shop> =
+        repository.getAllProducts()
+}
